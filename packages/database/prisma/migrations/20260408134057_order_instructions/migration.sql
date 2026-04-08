8bba17267e3004881570e7e1c6f3e7aeda371b02
Nicolò Spampatti <nicolo.spampa@gmail.com>
2026-04-08 22:20:09 +0200

    🛠️ Fixed db migration

--- packages/database/prisma/migrations/20260408134057_order_instructions/migration.sql
+++ packages/database/prisma/migrations/20260408134057_order_instructions/migration.sql
@@ -1,28 +1,5 @@
-/*
-  Warnings:
-
-  - You are about to drop the `banner` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
-DROP TABLE `banner`;
-
--- CreateTable
-CREATE TABLE `banners` (
-    `id` VARCHAR(191) NOT NULL,
-    `label` VARCHAR(191) NOT NULL,
-    `type` ENUM('EVENT', 'SPONSOR') NOT NULL,
-    `title` VARCHAR(191) NULL,
-    `description` VARCHAR(191) NULL,
-    `website` VARCHAR(191) NULL,
-    `instagram` VARCHAR(191) NULL,
-    `facebook` VARCHAR(191) NULL,
-    `image` VARCHAR(191) NULL,
-    `color` VARCHAR(191) NOT NULL DEFAULT 'fecc01',
-    `dateTime` DATETIME(3) NULL,
-
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- RenameTable
+RENAME TABLE `Banner` TO `banners`;
 
 -- CreateTable
 CREATE TABLE `order_instructions` (
